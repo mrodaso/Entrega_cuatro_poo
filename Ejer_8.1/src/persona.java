3339d1230dd1241e5c034f5c2c0c281844ec3c7d
mrodaso <142740232+mrodaso@users.noreply.github.com>
2023-10-07 13:11:55 -0500

    Add files via upload

---
+++ Ejer_8.1/src/persona.java
@@ -0,0 +1,16 @@
+
+public class persona {
+    private String nombre; 
+    private String apellidos;
+    private String telefono; 
+    private String direccion;
+    public persona(String nombre, String apellido, String telefono, String direccion) {
+        this.nombre=nombre;
+        this.apellidos = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        
+    }
+   
+}
+   
